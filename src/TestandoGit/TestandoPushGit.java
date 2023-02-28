b5b920f35bb7dcbab6c7c0fdea3848a3832cc8df
daniloramos1 <daniloramos@sapo.pt>
2023-02-27 22:32:23 -0300

    "Alteracao"

--- src/TestandoGit/TestandoPushGit.java
+++ src/TestandoGit/TestandoPushGit.java
@@ -3,7 +3,7 @@ package TestandoGit;
 public class TestandoPushGit {
 
 	public static void main(String[] args) {
-     System.out.println("Ola, este é apenas um teste para o git...");
+     System.out.println("teste dois");
 
 	}
 
